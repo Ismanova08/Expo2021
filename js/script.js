1094e0636883c499b9ffcac057e5d26e1d0cbbeb
ismanova08 <ismanova.mm@gmail.com>
2021-03-22 20:53:34 +0600

    add header + slader

--- js/script.js
+++ js/script.js
@@ -1,13 +1,66 @@
-$(document).on("ready", function () {
-  $(".center").slick({
-    infinite: true,
-    focusOnChange: true,
-    arrows: false,
-    focusOnSelect: true,
+$(document).ready(function () {
+  $(".slider").slick({
     centerMode: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    centerPadding: "60px",
+    slidesToShow: 4,
     speed: 1500,
-    centerPadding: "100px",
+    index: 2,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
+});
+
+$(".slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  centerMode: true,
+  variableWidth: true,
+  infinite: true,
+  focusOnSelect: true,
+  cssEase: "linear",
+  touchMove: true,
+  prevArrow: '<button class="slick-prev"> < </button>',
+  nextArrow: '<button class="slick-next"> > </button>',
+});
+//         responsive: [
+//             {
+//               breakpoint: 576,
+//               settings: {
+//                 centerMode: false,
+//                 variableWidth: false,
+//               }
+//             },
+//
+
+var imgs = $(".slider img");
+imgs.each(function () {
+  var item = $(this).closest(".item");
+  item.css({
+    "background-image": "url(" + $(this).attr("src") + ")",
+    "background-position": "center",
+    "-webkit-background-size": "cover",
+    "background-size": "cover",
+  });
+  $(this).hide();
 });
